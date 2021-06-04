743d63ff039c8c69fcf8c76b8205c319dc85fed5
xiaoxu9907 <xiaoxu9907@hotmail.com>
2021-06-05 00:00:17 +0800

    update

---
+++ process/1_preprocess_img.py
@@ -0,0 +1,121 @@
+import os
+import tifffile as tiff
+import pandas as pd
+import numpy as np
+import argparse 
+import math
+
+def combine_channels(images_path, panel_path, channel_type, out_path, imgsuffix, imagej=True):
+    images=[]
+    for fn in os.listdir(images_path):
+        if fn.endswith(imgsuffix):
+            images.append(fn)
+    print('process images: {}'.format(images))        
+    panel=pd.read_csv(panel_path)
+
+    for i in range(len(images)):
+        img=tiff.imread(os.path.join(images_path, images[i]))
+        if img.shape[0] > 3:
+            result=np.zeros((len(channel_type),img.shape[1],img.shape[2]))
+            for j in range(len(channel_type)):#["nuclear","membrane_cytoplasm"]
+                channels=list(panel[panel[channel_type[j]]==1].index)
+                result[j,:,:] = np.sum(img[channels,:,:],axis=0)
+            #print(result.shape)
+            tiff.imwrite(os.path.join(out_path,images[i].replace('full','combined')),result,imagej=imagej)
+
+
+def combine2Img(images_path, combined_path, panel_info, imgsuffix):
+#     images_path="/mnt/davinci/temp/kong_IMC/analysis/fullstacks/"
+#     combined_path="/mnt/davinci/temp/kong_IMC/analysis/unet/combine2"
+
+    if not os.path.exists(combined_path):
+        os.makedirs(combined_path)
+
+    combine_channels(images_path=images_path,
+                    panel_path = panel_info,
+                    channel_type=["nuclear","membrane_cytoplasm"],
+                    out_path=combined_path,
+                    imgsuffix = imgsuffix,
+                    imagej=False)
+
+def cropImg(crop512_combined_path, combined_path):
+    
+#     crop512_combined_path="/mnt/davinci/temp/kong_IMC/analysis/unet/crop512_combined2_image"
+    if not os.path.exists(crop512_combined_path):
+        os.makedirs(crop512_combined_path)
+
+    for file in os.listdir(combined_path):
+        img=tiff.imread(os.path.join(combined_path,file))
+        z, y, x = img.shape
+
+        if (y > 100) and (x > 100):
+            for i in range(0, x, 512):
+                for j in range(0, y, 512):
+                    pos = str(i)+'_'+str(j)
+                    img512 = img[:,i:i+512,j:j+512]
+                    z2, y2, x2 = img512.shape
+                    if (y2 < 512) or (x2 < 512):
+                        padimg= np.pad(img512, ((0,0), (0, 512-y2), (0, 512-x2)), 'constant', constant_values=(0, 0))
+                        img512 = padimg
+                tiff.imwrite(os.path.join(crop512_combined_path,file.replace("combinedstack.tiff","combined_cropx{}_cropy{}.tiff".format(str(i),str(j)))),img512,imagej=False)
+                            
+def mergeImg(prop_path, combined_path, out_path):
+    
+    files = os.listdir(prop_path)
+    files = [i for i in files if '_pred_Probabilities' in i]
+    
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    sampledic = {}
+
+    for each in files:
+        sample = each.split('_combined_cropx0')[0]
+        if sample not in sampledic.keys():
+            sampledic[sample] = [each]
+        else:
+            sampledic[sample].append(each)
+            
+    for each in sampledic.keys():
+        rois = sampledic[each]
+        imgs = {}
+        for roi in rois:
+            x = roi.split('_pred_Probabilities')[0].split('_crop')[1][1:]
+            y = roi.split('_pred_Probabilities')[0].split('_crop')[2][1:]
+            imgs[str(x)+'_'+str(y)] = tiff.imread(os.path.join(prop_path, roi))
+        original = tiff.imread(os.path.join(combined_path, each+'_combinedstack.tiff'))
+
+        z, y, x = original.shape
+        ori_prop = np.zeros((512*math.ceil(x/512),512*math.ceil(x/512),3))
+
+        for i in range(0, x, 512):
+            for j in range(0, y, 512):
+                pos = str(i)+'_'+str(j)
+                ori_prop[i:i+512,j:j+512] = imgs[pos]
+                
+
+        tiff.imwrite('{}/{}_pred_Probabilities.tiff'.format(out_path,each),ori_prop.astype('uint16'))
+                
+                
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--process', type=str, help='pre or post')
+    parser.add_argument('--workdir', type=str, default="/workspace/data/predict_BRCA2/", help='the path of the data folder')
+    parser.add_argument('--panel', type=str, default='/workspace/data/panel/BRCA2.csv', help='the path of the panel file => same ordered with img channel: at least need contain two columns("nuclear","membrane_cytoplasm")')
+    parser.add_argument('--imgsuf', type=str, default='_fullstack.tiff', help='images suffix')
+    args = parser.parse_args()
+    
+    workdir = args.workdir
+    process = args.process
+    panelpath = args.panel
+    imgsuffix = args.imgsuf
+    
+    if process == 'pre':
+        combine2Img(os.path.join(workdir, 'fullstacks'), os.path.join(workdir, 'combined2_image'), panelpath, imgsuffix)
+        cropImg(os.path.join(workdir, 'crop512_combined2_image'), os.path.join(workdir, 'combined2_image'))
+    elif process == 'post':
+        prop_path = os.path.join(workdir, 'predictionProbability','threshold-99.7_withAugnoise-0.5')
+        combined_path = os.path.join(workdir, 'combined2_image')
+        out_path = os.path.join(prop_path, 'ori_prop')
+        mergeImg(prop_path, combined_path, out_path)
+    
---
+++ process/3_imc_extract_pro.py
@@ -0,0 +1,126 @@
+from warnings import filterwarnings 
+filterwarnings('ignore')
+
+from multiprocessing import Process, Pool
+import os, time, random
+import numpy as np
+import scipy.ndimage as ndi
+from skimage import measure,color
+import os
+import skimage.io as io
+import skimage.transform as trans
+import matplotlib.pyplot as plt
+import random
+import cv2
+import tifffile as tiff
+from PIL.Image import fromarray as show
+import pandas as pd
+from tqdm import tqdm
+import time
+import argparse
+
+
+def measure_markers(raw_img,raw_mask,measure_label=False):
+    if measure_label:
+        labels=measure.label(raw_mask,connectivity=2)
+    else:
+        labels=raw_mask
+        
+    props = measure.regionprops(labels)
+     
+    mean_exp_list=[]
+    labels_index=list(np.unique(labels))
+    labels_index.pop(0)
+    cell_pos = []
+    cell_area = []
+    cell_perimeter = []
+    cell_minor_axis = []
+    cell_major_axis = []
+    for i in labels_index:
+        imask=raw_mask.copy()
+        imask[labels!=i]=0
+        imask[labels==i]=255
+        n_pixel=len(imask[imask==255])
+        imasked = cv2.add(raw_img, np.zeros(np.shape(raw_img), dtype=np.float64), mask=imask.astype(np.uint8))
+        #avg=np.mean(imasked,axis=(0,1))
+        avg=np.sum(imasked,axis=(0,1))/n_pixel
+        mean_exp_list.append(avg)
+        cell_pos.append(props[i-1].centroid)
+        cell_area.append(props[i-1].area)
+        cell_perimeter.append(props[i-1].perimeter)
+        cell_minor_axis.append(props[i-1].minor_axis_length)
+        cell_major_axis.append(props[i-1].major_axis_length)
+        
+        
+    mean_exp_matrix=np.matrix(mean_exp_list)
+    
+    labels_prop = {'index':labels_index, 'pos':cell_pos, 'area':cell_area, 'perimeter':cell_perimeter, 'minor_axis':cell_minor_axis,
+                  'major_axis':cell_major_axis}
+    return labels_prop,mean_exp_matrix
+
+
+def fun2(i):
+    if os.path.exists(os.path.join(output_path,"{0}.csv".format(i))):
+        pass
+    else:
+        raw_img=tiff.imread(os.path.join(img_path,i+img_suffix)).transpose(1,2,0).astype(np.float64)
+        raw_mask = tiff.imread(os.path.join(mask_path,i+mask_suffix))
+        labels_prop,mean_exp_matrix= measure_markers(raw_img=raw_img,raw_mask=raw_mask,measure_label=True)
+        
+        marker_exp=pd.DataFrame(mean_exp_matrix,columns=fullstacks_channel_names['marker'])
+        #marker_exp.to_csv(os.path.join(output_path,"{0}.csv".format(images[i].split('.')[0])))
+
+        #marker_exp['ImageNumber']=images[i].split('_')[2]
+        marker_exp['ObjectNumber']=labels_prop['index']
+        marker_exp['Position']=labels_prop['pos']
+        marker_exp['area'] = labels_prop['area']
+        marker_exp['perimeter'] = labels_prop['perimeter']
+        marker_exp['minor_axis'] = labels_prop['minor_axis']
+        marker_exp['major_axis']= labels_prop['major_axis']
+        #marker_exp['metabricId'] = images[i].split('_')[0].replace('MB','MB-')
+        #marker_exp['core_id'] = images[i].split('_')[1]
+
+        #marker_exp.to_csv(os.path.join(output_path,"{0}.csv".format(images[i].split('.')[0])))
+        marker_exp.to_csv(os.path.join(output_path,"{0}.csv".format(i)))
+        print(os.path.join(output_path,"{0}.csv".format(i)))
+        return marker_exp
+    
+    
+####  
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--workdir', type=str, default="/workspace/data/predict_BRCA2/", help='the path of the data folder')
+    parser.add_argument('--panel', type=str, default='/workspace/data/panel/BRCA2.csv', help='the path of the panel file => same ordered with img channel: at least need contain two columns("nuclear","membrane_cytoplasm")')
+    parser.add_argument('--imgsuf', type=str, default='_fullstack.tiff', help='images suffix')
+    parser.add_argument('--maskpath', type=str, default='/workspace/data/predict_BRCA2/mask')
+    args = parser.parse_args()
+    
+    workdir = args.workdir
+    panelpath = args.panel
+    mask_path = args.maskpath
+    output_path = os.path.join(workdir, 'protein')
+    
+    fullstacks_channel_names = pd.read_csv(panelpath)
+    
+    img_path = os.path.join(workdir, 'fullstacks')
+    img_suffix = args.imgsuf
+    
+    masks=np.sort(os.listdir(mask_path))
+    mask_suffix = '_pred_Probabilities_cell_mask.tiff'
+    masks = [i for i in masks if 'cell_mask' in i]
+    
+    id1 = [i.split(img_suffix)[0] for i in os.listdir(img_path) if 'fullstack' in i]
+    id2 = [i.split(mask_suffix)[0] for i in os.listdir(mask_path) if 'cell_mask' in i]
+    img_name = list(set(id1) & set(id2))
+    image_name = np.sort(img_name)
+    print('process {} images'.format(len(image_name)))
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    t0 = time.time()
+    with Pool(40) as p:
+        result2 = p.map(fun2,[i for i in image_name])
+    print(time.time() - t0, "seconds process time")
+    
